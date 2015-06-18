de1ca08278a17414493013155a35e9a32d8800ea
sjacker <sjacker@umich.edu>
2015-06-17 22:07:23 -0400

    added map and camera intent

--- app/src/main/java/com/example/sjacker/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/sjacker/sunshine/app/MainActivity.java
@@ -3,10 +3,14 @@ package com.example.sjacker.sunshine.app;
 // import android.app.Fragment;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -42,10 +46,53 @@ public class MainActivity extends ActionBarActivity {
             return true;
         }
 
+        if (id == R.id.action_preferred_location) {
+            showPreferredLocationOnMap();
+            return true;
+        }
+
+        if (id == R.id.action_selfie) {
+            launchCameraSelfieMode();
+            return true;
+        }
+
         return super.onOptionsItemSelected(item);
     }
 
+    private void showPreferredLocationOnMap() {
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String location = sharedPrefs.getString(getString(R.string.pref_location_key),
+                getString(R.string.default_zip));
 
+        Uri geoLocation = Uri.parse("geo:0,0?").buildUpon()
+                .appendQueryParameter("q", location)
+                .build();
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+
+        else {
+            Toast.makeText(getApplicationContext(), "Location Invalid, Try Again Sucker", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    private void launchCameraSelfieMode() {
+
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+
+        else {
+            Toast.makeText(getApplicationContext(), "lol you don't have a camera on yo phone", Toast.LENGTH_LONG).show();
+        }
+    }
 
 }
 
